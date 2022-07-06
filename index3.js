06ef597fa67b386a5025de311aaa94c4915c6d55
nanogutierrez <105751221+nanogutierrez@users.noreply.github.com>
2022-07-05 22:46:40 -0300

    Add files via upload

---
+++ index3.js
@@ -0,0 +1,27 @@
+
+for(let i = 1; i <= 40; i++){
+      let nombre = prompt("ingrese su nombre y apellido").toUpperCase();
+      let mensaje = `Asiento n: ${i} Nombre: ${nombre}`;
+      alert(mensaje);
+}
+
+// switch(i){
+//       case 1:
+//             console.log("tenes un 15% de descuento en tu ticket");
+//             break;
+//       // case 10:
+//       //       console.log("tenes un 15% de descuento en tu ticket");
+//       //       break;
+//       // default:
+//       //       console.log("no tenes un 15% de descuento en tu ticket");
+//       //       break;
+// }
+
+//  let entrada = prompt("Ingresar una letra");
+//       let salida  = entrada + " " + "ingresada";
+//        alert(salida);
+
+
+
+
+
